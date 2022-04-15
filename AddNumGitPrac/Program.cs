c20bcfcb2b554ec9984a608eead2ddb949559c39
Darsh142001 <darshikapillay6@gmail.com>
2022-04-15 11:49:35 +0200

    Initial commit adding two numbers to develop

--- AddNumGitPrac/Program.cs
+++ AddNumGitPrac/Program.cs
@@ -4,9 +4,15 @@ namespace AddNumGitPrac
 {
     class Program
     {
+        
+        public static int a = 10;
+        public static int b = 12;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine(a + b);
         }
     }
 }
